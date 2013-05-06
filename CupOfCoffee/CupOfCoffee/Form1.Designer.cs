dd28f7204cd03fbb380838a6b6d7ce44ca5115c6
bojordan <bo@bojordan.com>
2013-05-06 08:34:21 -0400

    Changed timer interval from 1s back to 60s

--- CupOfCoffee/CupOfCoffee/Form1.Designer.cs
+++ CupOfCoffee/CupOfCoffee/Form1.Designer.cs
@@ -54,7 +54,7 @@
             // 
             // timer1
             // 
-            this.timer1.Interval = 1000;
+            this.timer1.Interval = 60000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // btnStop
